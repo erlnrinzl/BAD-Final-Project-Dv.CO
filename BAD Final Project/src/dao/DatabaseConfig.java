1f5450cdef34e2e54089e5e49d0dd6b09096bc68
erlnrinzl <henrichfergian@gmail.com>
2024-12-13 09:57:38 +0700

    refractor database config to make connection on initial only

--- BAD Final Project/src/dao/DatabaseConfig.java
+++ BAD Final Project/src/dao/DatabaseConfig.java
@@ -8,8 +8,17 @@ public abstract class DatabaseConfig {
     private static final String URL = System.getenv("DB_URL");
     private static final String USER = System.getenv("DB_USER");
     private static final String PASSWORD = System.getenv("DB_PASS");
+    
+    private static Connection connection;
 
+    private static void init() throws SQLException {
+		connection = DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+    
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        if (connection == null) {
+			init();
+		}
+        return connection;
     }
 }
